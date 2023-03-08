d530873f66ed4337eaa792ea9507069ce978c2a4
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-03-08 15:37:07 -0300

    auto-wiring annotations

---
+++ src/main/java/com/parkus/autowiring/annotations/Employee.java
@@ -0,0 +1,35 @@
+package com.parkus.autowiring.annotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+
+    private Address address;
+
+    public Employee() {
+    }
+
+    public Employee(Address address) 
+    {
+        this.address = address;
+    }
+
+    public Address getAddress() 
+    {
+        return address;
+    }
+
+    @Autowired
+    public void setAddress(Address address) 
+    {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Employee [address=" + address + "]";
+    }
+
+    
+}
